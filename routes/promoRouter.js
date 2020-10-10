2d15f63748619d87b47535fedfe9a473ec905c62
Mridul Jain <itsme.mriduljain@gmail.com>
2020-10-10 23:08:08 +0530

    populate

--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Promos = require('../models/promotions');
 const promoRouter = express.Router();
+var authenticate = require('../authenticate');
 promoRouter.use(bodyParser.json());
 
 
@@ -18,21 +19,22 @@ promoRouter.route('/')
     .catch((err) => next(err));
 
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser,(req, res, next) => {
     Promos.create(req.body)
-    .then((Promos) => {
-        console.log('Promo Created ', Promos);
+    .then((Promo) => {
+        console.log('Promo Created ', Promo);
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        res.json(Promos);
+        res.json(Promo);
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.put((req, res, next) => {
+
+.put(authenticate.verifyUser,(req, res, next) => {
     res.statusCode = 403;
-    res.end('PUT operation not supported on /promos');
-})
-.delete((req, res, next) => {
+    res.end('PUT operation not supported on /dishes');
+    })
+.delete(authenticate.verifyUser,(req, res, next) => {
     Promos.remove({})
     .then((Promos) => {
         res.statusCode = 200;
@@ -52,11 +54,11 @@ promoRouter.route('/:promoId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser,(req, res, next) => {
     res.statusCode = 403;
     res.end('POST operation not supported on /promos/'+ req.params.promoId);
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser,(req, res, next) => {
     Promos.findByIdAndUpdate(req.params.promoId, {
         $set: req.body
     }, { new: true })
@@ -67,7 +69,7 @@ promoRouter.route('/:promoId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser,(req, res, next) => {
     Promos.findByIdAndRemove(req.params.promoId)
     .then((Promo) => {
         res.statusCode = 200;
